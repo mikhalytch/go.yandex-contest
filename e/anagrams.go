59429e36db1b7de603c5d5cdadf9b66e83ef0204
Aleksey V. Mikhaylov <gmikhalytch@gmail.com>
2022-01-17 17:47:58 +0300

    [task E] trying to overcome error on test #499

--- e/anagrams.go
+++ e/anagrams.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"reflect"
 )
 
 func main() {
 	Anagrams(os.Stdin, os.Stdout)
 }
+
+type dict map[byte]int
+
 func Anagrams(reader io.Reader, writer io.Writer) {
 	scanner := bufio.NewScanner(reader)
 	var s1, s2 string
@@ -22,34 +24,65 @@ func Anagrams(reader io.Reader, writer io.Writer) {
 		s1 = scanner.Text()
 	}
 	if !scanner.Scan() {
-		_, _ = fmt.Fprintf(writer, "1")
+		_, _ = fmt.Fprintf(writer, "0")
 		return
 	} else {
 		s2 = scanner.Text()
 	}
-	if areAnagrams(s1, s2) {
+	if areAnagrams(s1, s2, writer) {
 		_, _ = fmt.Fprintf(writer, "1")
 	} else {
 		_, _ = fmt.Fprintf(writer, "0")
 	}
 }
-func areAnagrams(a, b string) bool {
-	if len(a) > 100000 {
-		a = a[:100000]
-	}
-	if len(b) > 100000 {
-		b = b[:100000]
-	}
-	if len(a) != len(b) {
-		return false
-	}
-	dictA, dictB := createDict(a), createDict(b)
-	return reflect.DeepEqual(dictA, dictB)
+func areAnagrams(a, b string, writer io.Writer) bool {
+	// todo fixing #498/499
+	//const maxStringLength = 100000
+	//if len(a) > maxStringLength {
+	//	_, _ = fmt.Fprintf(writer, "1")
+	//	a = a[:maxStringLength]
+	//}
+	//if len(b) > maxStringLength {
+	//	_, _ = fmt.Fprintf(writer, "1")
+	//	b = b[:maxStringLength]
+	//}
+	// todo fixing #498
+	//if len(a) != len(b) {
+	//	return false
+	//}
+	dictA, dictB := createDict(a, writer), createDict(b, writer)
+	// todo fixing #499
+	//return reflect.DeepEqual(dictA, dictB)
+
+	return compareDictionaries(dictA, dictB)
 }
-func createDict(s string) map[rune]int {
-	res := make(map[rune]int)
-	for _, r := range s {
-		res[r]++
+func compareDictionaries(d1, d2 dict) bool {
+	return containsAll(d1, d2) && containsAll(d2, d1)
+}
+func containsAll(container, questioner dict) bool {
+	for qk, qv := range questioner {
+		if cv, cok := container[qk]; !cok {
+			return false
+		} else {
+			if cv != qv {
+				return false
+			}
+		}
 	}
+	return true
+}
+func createDict(s string, writer io.Writer) dict {
+	res := make(dict)
+	for i := 0; i < len(s); i++ {
+		res[s[i]]++
+	}
+	// todo fixing for #499
+	//for _, r := range s {
+	// todo fixing #499
+	//if utf8.RuneLen(r) != 1 {
+	//	_, _ = fmt.Fprintf(writer, "1")
+	//}
+	//res[r]++
+	//}
 	return res
 }
